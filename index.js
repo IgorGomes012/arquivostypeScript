38b3ec4586e76ee71b4fa88e3a2d193bd4cb3b66
igorGomes <gomesoliveirasl02@hotmail.com>
2022-05-03 09:20:40 -0300

    começando com typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+function greet(name) {
+    return "ola ".concat(name);
+}
+console.log(greet("igor"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function greet(name:string){
+    return `ola ${name}`
+}
+
+console.log(greet(`igor`))
